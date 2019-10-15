f43e11854325a2992ae566778b1e13ffa5e32b87
Cedric Druck <ced@4gclinical.com>
2019-10-15 10:45:36 +0200

    euler10 and primes

--- add_primes.py
+++ add_primes.py
@@ -1,8 +1,9 @@
 from utils import generate_n_primes, generate_primes_up_to
+import itertools
 
 MORE_PRIMES = 10000
 
-def main():
+def generate():
     batch = 20
     more_primes = MORE_PRIMES
     prime_file = open("primes", 'r')
@@ -19,6 +20,18 @@ def main():
         print(list(primes[-batch:]))
         more_primes -= batch
 
+def read_utm():
+    f = open('primes_utm_edu.txt', 'r')
+    primes = f.read()
+    primes = primes.split('\n')[2:]
+    primes = [list(filter(lambda l: len(l)>0, line.split(' '))) for line in primes]
+    primes = list(itertools.chain.from_iterable(primes))
+
+    f = open('primes', 'w+')
+    f.write('\n'.join(primes))
+
+def main():
+    read_utm()
 
 main()
 
--- euler10.py
+++ euler10.py
@@ -2,7 +2,8 @@ from utils import read_n_primes
 
 
 def main():
-    #primes = read_n_primes()
+    primes = read_n_primes(148933)
+    print(sum(primes))
     pass
 
 main()
