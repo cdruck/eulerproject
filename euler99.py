61ce55f6db88d969100f180f16e70b592df8b685
Cedric Druck <ced@4gclinical.com>
2019-10-23 22:51:42 +0200

    euler 99

--- euler99.py
+++ euler99.py
@@ -1,6 +1,21 @@
-
+from math import log10
 
 def main():
-    pass
+    be = [tuple(map(int, l.split(','))) for l in open('p099_base_exp.txt').read().split('\n')]
+
+    # this is pretty obvious ... numbers are too big to be computed as such, but we can map them to a
+    # monotonous function, in particular log (in any base, I picked 10), which has the property that
+    # log(a**b) == b * log(a) which is much more manageable.
+    # comparing the log values will give the correct line number ...
+
+    max_n = 0
+    max_log = -1e99
+    for n, (b, e) in enumerate(be):
+        curlog = e*log10(b)
+        if max_log < curlog:
+            max_n = n
+            max_log = curlog
+
+    print(max_n + 1)
 
 main()
